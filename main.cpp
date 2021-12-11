19fe3a1518fc3228974e30c4f944bd425261c0d8
Sean Garrett Loveland <seangloveland@gmail.com>
2021-12-10 18:47:14 -0800

    Added logic for bubble sort and selection sort

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 const unsigned WINDOW_WIDTH = 1000;
 const unsigned WINDOW_HEIGHT = 1000;
 const int SORTABLE_COUNT = 100;
+int delay = 100;
 bool isPaused = false;
 
 sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Visualizer");
@@ -18,6 +19,8 @@ void drawSortable(SortObject sortables[]);
 void swap(SortObject &bar1, SortObject &bar2);
 
 void insertionSort(SortObject sortables[]);
+void bubbleSort(SortObject sortables[]);
+void selectionSort(SortObject sortables[]);
 
 int main() {
     SortObject sortables[SORTABLE_COUNT];
@@ -31,7 +34,9 @@ int main() {
         }
 
         if(!isPaused)
-            insertionSort(sortables);
+            selectionSort(sortables);
+            //insertionSort(sortables);
+            //bubbleSort(sortables);
     }
 
 
@@ -75,7 +80,7 @@ void insertionSort(SortObject sortables[]) {
         key = sortables[i];
         j = i - 1;
 
-        //sf::sleep(sf::milliseconds(150));
+        sf::sleep(sf::milliseconds(delay));
         drawSortable(sortables);
 
         while (j >= 0 && sortables[j] > key) {
@@ -83,19 +88,101 @@ void insertionSort(SortObject sortables[]) {
             sortables[j + 1] = sortables[j];
             sortables[j] = key;
 
-            //sf::sleep(sf::milliseconds(150));
+            sf::sleep(sf::milliseconds(delay));
             drawSortable(sortables);
             --j;
         }
         sortables[j + 1] = key;
         sortables[j+1].setColor(sf::Color::Green);
-        //sf::sleep(sf::milliseconds(150));
+        sf::sleep(sf::milliseconds(delay));
         drawSortable(sortables);
     }
 
     isPaused = true;
 }
 
+void bubbleSort(SortObject sortables[]) {
+    for(int i = SORTABLE_COUNT - 1; i >= 0; --i) {
+        for(int j = 0; j < i; ++j) {
+            if(sortables[j] > sortables[j+1]) {
+                //color both bars to be swapped as the color red
+                sortables[j].setColor(sf::Color::Red);
+                sortables[j + 1].setColor(sf::Color::Red);
+
+                //draw the array with the red bars before swap
+                sf::sleep(sf::milliseconds(delay));
+                drawSortable(sortables);
+
+                swap(sortables[j], sortables[j + 1]);
+
+                sf::sleep(sf::milliseconds(delay));
+                drawSortable(sortables);
+
+                sortables[j].setColor(sf::Color::White);
+                sortables[j + 1].setColor(sf::Color::White);
+
+                //display the array after the swap
+                sf::sleep(sf::milliseconds(delay));
+                drawSortable(sortables);
+            }
+        }
+        sortables[i].setColor(sf::Color::Green);
+        sf::sleep(sf::milliseconds(delay));
+        drawSortable(sortables);
+    }
+    drawSortable(sortables);
+}
+
+void selectionSort(SortObject sortables[]) {
+    int minIndex;
+
+    for(int i = 0; i < SORTABLE_COUNT - 1; ++i) {
+        isPaused = true;
+        minIndex = i;
+        sortables[minIndex].setColor(sf::Color::Blue);
+        drawSortable(sortables);
+
+        for(int j = i + 1; j < SORTABLE_COUNT; ++j) {
+            if(sortables[j] < sortables[minIndex]) {
+                sortables[minIndex].setColor(sf::Color::White);
+
+                minIndex = j;
+
+                sortables[minIndex].setColor(sf::Color::Blue);
+                sf::sleep(sf::milliseconds(delay));
+                drawSortable(sortables);
+            }
+        }
+        if(minIndex != i) {
+            sortables[i].setColor(sf::Color::Red);
+            sortables[minIndex].setColor(sf::Color::Red);
+            sf::sleep(sf::milliseconds(delay));
+            drawSortable(sortables);
+
+            swap(sortables[minIndex], sortables[i]);
+
+            sf::sleep(sf::milliseconds(delay));
+            drawSortable(sortables);
+
+            sortables[minIndex].setColor(sf::Color::White);
+            sortables[i].setColor(sf::Color::Green);
+
+            sf::sleep(sf::milliseconds(delay));
+            drawSortable(sortables);
+        }
+        else {
+            sortables[i].setColor(sf::Color::Green);
+            sf::sleep(sf::milliseconds(delay));
+            drawSortable(sortables);
+        }
+    }
+
+    sortables[SORTABLE_COUNT - 1].setColor(sf::Color::Green);
+    drawSortable(sortables);
+}
+
 void swap(SortObject &bar1, SortObject &bar2) {
-    SortObject temp;
+    SortObject temp = bar1;
+    bar1 = bar2;
+    bar2 = temp;
 }
